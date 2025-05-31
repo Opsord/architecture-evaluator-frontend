9888b7b3acf4dd0fae134ee6e54f403c013dc37f
Andres Zelaya <andres.zelaya.d@hotmail.com>
2025-05-31 03:48:07 -0400

    Changed footer

--- architecture-evaluator-frontend/src/components/Footer.tsx
+++ architecture-evaluator-frontend/src/components/Footer.tsx
@@ -2,34 +2,25 @@ import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-background-dark rounded-lg shadow-sm m-4">
-            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 text-gray-light rounded-lg cursor-pointer hover:bg-gray-light dark:hover:bg-gray-dark dark:hover:text-white">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/"
-                       className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
-                        <span
-                                className="self-center text-2xl font-semibold whitespace-nowrap text-gray-light">Flowbite</span>
-                    </a>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-light sm:mb-0">
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <hr className="my-6 border-gray-border sm:mx-auto lg:my-8"/>
-                <span className="block text-sm text-gray-light sm:text-center">© 2023 <a
-                    href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-            </div>
+        <footer
+            className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow-sm md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
+                                                                                        className="hover:underline">Flowbite™</a>. All Rights Reserved.
+    </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
         </footer>
     );
 }
